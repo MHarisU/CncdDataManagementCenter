182297754ab01f60982571c8ea0b2065a13dc4b8
MHarisU <rhunar96@gmail.com>
2021-11-09 16:46:35 +0500

    9th November 2021 - > Working of Baseline Form for Recruitment (Remaining)

--- app/src/main/java/com/cncd/first/UIs/BaselineQuestionnaireRecruitmentActivity.java
+++ app/src/main/java/com/cncd/first/UIs/BaselineQuestionnaireRecruitmentActivity.java
@@ -3,6 +3,7 @@ package com.cncd.first.UIs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.cncd.first.R;
 
@@ -12,5 +13,9 @@ public class BaselineQuestionnaireRecruitmentActivity extends AppCompatActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baseline_questionnaire_recruitment);
+    }
+
+    public void CloseForm(View view) {
+        finish();
     }
 }
